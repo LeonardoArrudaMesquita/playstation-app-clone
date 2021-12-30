ea382293679ba2fa9077272b974a5abf1d8ab3c0
LeonardoArrudaMesquita <leonardoarruda@outlook.com.br>
2021-12-30 01:41:41 -0300

    feat: adding a palette colors

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    white: '#E8EAEE',
+    black1: '#1A191F',
+    black2: '#000000',
+    grey1: '#485258',
+    grey2: '#4F4D53',
+    grey3: '#2F353B',
+  },
+};
